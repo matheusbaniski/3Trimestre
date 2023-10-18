55c85d212bff8757f8f5b5880c5149cccb763623
matheusbaniski <127850821+matheusbaniski@users.noreply.github.com>
2023-10-18 09:43:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-    function tocaSomPom() {
-      document.querySelector('#som_tecla_pom').play();}
+    function tocaSom() {
+      document.querySelector(idElementoAudio).play();}
+
 
 document.querySelectorAll('.tecla');
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
-//estrair de repetiçao - Enquanto
+// Estrutura de repetiçao - Enquanto
 while(contador <ListaDeTeclas.length) {
-  ListaDeTeclas[contador].onclick= tocaSomPom;
-  contador = contador+1;
+  listadeTeclas [contador].classList [1];
+  const tecla = listadeTeclas[contador]
+  const instrumento = tecla.classList [1];
+  '#som_${instrumento}';
+  console.log (instrumento);
+  listaDeTeclas [contador].onclick = function () {
+ tocaSom ('#som_tecla_splash');
+}
+contador = contador + 1;
 }
